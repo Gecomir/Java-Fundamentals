9af11d26c710520895f8178aaccb0e3cf67f7a74
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-03-24 18:44:31 +0200

    Add files via upload

---
+++ Methods/NxNMatrix.java
@@ -0,0 +1,23 @@
+package Methods_exercises;
+
+import java.util.Scanner;
+
+public class NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        printNXnMatrix(number);
+    }
+
+    public static void printNXnMatrix(int number) {
+        for (int i = 1; i <= number; i++) {
+
+            for (int j = 1; j <= number; j++) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+}
