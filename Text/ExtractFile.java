ec65ed0e58122f186e3238f38ec8e23df52aa220
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-04-23 12:27:09 +0300

    Add files via upload

---
+++ Text/ExtractFile.java
@@ -0,0 +1,16 @@
+package Text_exercises;
+
+import java.util.Scanner;
+
+public class ExtractFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String file = scanner.nextLine();
+
+        String fileName = file.substring(file.lastIndexOf("\\") + 1, file.indexOf("."));
+        String fileExtension = file.substring(file.indexOf(".") + 1);
+        System.out.printf("File name: %s%n", fileName);
+        System.out.printf("File extension: %s%n", fileExtension);
+    }
+}
