bd9e9781aec30026a6a513275c27e93c3b9921a5
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-05-07 21:42:52 +0300

    Add files via upload

---
+++ IntegerAndRealNumber/ReversedChars.java
@@ -0,0 +1,16 @@
+package IntegerAndRealNumber;
+
+import java.util.Scanner;
+
+public class ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+        String three = scanner.nextLine();
+
+        String result = three + " " + second + " " + first;
+        System.out.println(result);
+    }
+}
